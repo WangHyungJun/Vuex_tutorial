d220ecf712947fb472f534901dbfb47de3ed23c4
WangHyungJun <whj2013123218@gmail.com>
2019-03-14 09:38:26 +0900

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
+</template>
+
+<script>
+  import ProductListOne from './components/ProductListOne';
+  import ProductListTwo from './components/ProductListTwo'
+
+export default {
+  components:{
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  data () {
+    return {
+      products: [
+        {name: 'Banana Skin', price: 20},
+        {name: 'Shiny Star', price: 40},
+        {name: 'Green Shells', price: 60},
+        {name: 'Red Shells', price: 80}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  body{
+    font-family: Ubuntu;
+    color: #555;
+  }
+</style>
